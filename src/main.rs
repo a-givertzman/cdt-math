4119ac11cf0bdbe07a94b8ab18b0ebd05f3abc23
a-givertzman <a.givertzman@icloud.com>
2024-10-08 15:44:40 +0300

    template

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+mod kernel;
+mod app;
 #[cfg(test)]
 mod tests;
+use app::app::App;
+use kernel::run::Run;
+use debugging::session::debug_session::{Backtrace, DebugSession, LogLevel};
 ///
 /// Application entry point
 fn main() {
-    println!("Hello, world!");
+    DebugSession::init(LogLevel::Debug, Backtrace::Short);
+    let path = "config.yaml";
+    let mut app = App::new(path);
+    if let Err(err) = app.run() {
+        log::error!("main | Error: {:#?}", err);
+    };
 }
