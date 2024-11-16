f443e47f0bbb94f3b153afa820e87ccda5c4cc3a
Klaimmoro <klaimmor228@gmail.com>
2024-11-16 15:00:09 +0300

    Back | Algorithm | Dynamic coefficient

---
+++ src/algorithm/dynamic_coefficient/dynamic_coefficient.rs
@@ -0,0 +1,42 @@
+use crate::{algorithm::{lifting_speed::lifting_speed::LiftingSpeed, select_bet_phi::select_bet_phi::SelectBetPhi}, kernel::{dbgid::dbgid::DbgId, str_err::str_err::StrErr}};
+///
+/// Класс, реализующий расчёт динамического коэффициента
+/// - 'select_bet_phi' - экземпляр класса [SelectBetPhi]
+/// - 'lifting_speed' - экземпляр класса [LiftingSpeed]
+/// [reference to dynamic coefficient documentation](design\docs\algorithm\part02\chapter_01_choose_hook.md)
+#[derive(Debug, Clone)]
+pub struct DynamicCoefficient {
+    dbgid: DbgId,
+    select_bet_phi: SelectBetPhi,
+    lifting_speed: LiftingSpeed,
+    value: Option<f64>,
+}
+impl DynamicCoefficient {
+    ///
+    /// Конструктор класса DynamicCoefficient
+    /// - 'select_bet_phi' - экземпляр класса [SelectBetPhi]
+    /// - 'lifting_speed' - экземпляр класса [LiftingSpeed]
+    pub fn new(select_bet_phi: SelectBetPhi, lifting_speed: LiftingSpeed) -> Self {
+        Self { dbgid: DbgId(format!("DynamicCoefficient")), select_bet_phi, lifting_speed, value: None }
+    }
+    ///
+    /// Метод расчёта динамического коэффициента
+    pub fn eval(&mut self) -> Result<f64, StrErr> {
+        match self.value {
+            Some(val) => Ok(val),
+            None => match self.select_bet_phi.eval() {
+                Ok(lift_class) => {
+                    match self.lifting_speed.eval() {
+                        Ok(vh) => {
+                            let value = lift_class.phi + lift_class.bet * vh;
+                            self.value = Some(value);
+                            Ok(value)
+                        }
+                        Err(err) => Err(err)
+                    }
+                }
+                Err(err) => Err(err),
+            }
+        }
+    }
+}
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,3 +1,4 @@
+pub mod dynamic_coefficient;
 pub mod lifting_speed;
 pub mod select_bet_phi;
 pub mod storage;
