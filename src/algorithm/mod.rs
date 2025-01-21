e2d6c540c26367682c7124e853a3b6fb4de3bcc2
Klaimmoro <klaimmor228@gmail.com>
2025-01-21 20:51:38 +0300

    Back | Algorithm | Rope effort

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -2,4 +2,5 @@ pub mod bearing_filter;
 pub mod dynamic_coefficient;
 pub mod hook_filter;
 pub mod lifting_speed;
+pub mod rope_effort;
 pub mod select_betta_phi;
--- src/tests/unit/algorithm/mod.rs
+++ src/tests/unit/algorithm/mod.rs
@@ -2,4 +2,5 @@ mod bearing_filter_test;
 mod dynamic_coefficient_test;
 mod hook_filter_test;
 mod lifting_speed_test;
+mod rope_effort_test;
 mod select_betta_phi_test;
