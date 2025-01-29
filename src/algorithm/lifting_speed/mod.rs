b98168e18d246746fde20b1fdbd3a39b5fcbc585
Zack <klaimmor228@gmail.com>
2025-01-29 15:52:16 +0300

    Back | Algorithm | Steady state lifting speed

--- src/algorithm/lifting_speed/mod.rs
+++ src/algorithm/lifting_speed/mod.rs
@@ -1,1 +1,17 @@
+//! # Module of calculating steady state lifting speed
+//!
+//! This module constist method of calculating steady state lifting speed
+//! and their processing in the system. 
+//! [reference to steady-state lifting speed documentation](design\docs\algorithm\part02\chapter_01_choose_hook.md)
+//! It includes:
+//! - calculating steady state lifting speed (`steady_state_lifting_speed`)
+//! - calculating half of nominal lifting speed of the mechanism (`payload_weight`)
+//!
+//! ## Example of using
+//! ```rust
+//! use crate::algorithm::lifting_speed::lifting_speed::LiftingSpeed;
+//! use crate::kernel::initial_data::initial_data::InitialData;
+//! let lifting_speed = LiftingSpeed::new(InitialData::new()).eval();
+//! println!("Steady state lifiting speed: {}", total);
+//! ```
 pub mod lifting_speed;
---
+++ src/kernel/initial_data/mod.rs
@@ -0,0 +1,12 @@
+//! # Module of struct that store initial data
+//!
+//! This module constist structure of initial data
+//! [documentation to initial data](design\docs\algorithm\part01\initial_data.md)
+//! 
+//! ## Example of using
+//! ```rust
+//! use crate::kernel::initial_data::initial_data::InitialData;
+//! let initial_data = InitialData::new();
+//! println!("Steady state lifiting speed: {}", total);
+//! ```
+pub mod initial_data;
