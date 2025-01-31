0795900b7f0c57437596dd01afdcbf56ce86e60d
Zack <klaimmor228@gmail.com>
2025-01-31 19:20:58 +0300

    Back | Algorithm | Select β2 and ϕ2

--- src/algorithm/entities/bet_phi.rs
+++ src/algorithm/entities/bet_phi.rs
@@ -1,11 +1,11 @@
 ///
 /// Struct to storage β2 and ϕ2 coefficients
 /// [reference to β2 and ϕ2 coefficients documentation](design\docs\algorithm\part02\chapter_01_choose_hook.md)
-/// - 'bet' - β2 coefficient
-/// - 'phi' - ϕ2 coefficient
 #[derive(Debug, Clone, PartialEq)]
 pub struct BetPhi {
+    /// β2 coefficient
     pub bet: f64,
+    /// -ϕ2 coefficient
     pub phi: f64,
 }
 //
