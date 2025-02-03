8be5f217440567121b7427a018d71d454ac40a03
Zack <klaimmor228@gmail.com>
2025-02-03 19:52:03 +0300

    modified:   src/algorithm/mod.rs 	new file:   src/algorithm/select_betta_phi/mod.rs 	new file:   src/algorithm/select_betta_phi/select_betta_phi.rs 	new file:   src/algorithm/select_betta_phi/select_betta_phi_ctx.rs 	modified:   src/tests/unit/algorithm/mod.rs 	new file:   src/tests/unit/algorithm/select_betta_phi_test.rs 	modified:   src/algorithm/context/context.rs 	modified:   src/algorithm/entities/mod.rs 	modified:   src/algorithm/initial_ctx/initial_ctx.rs

---
+++ src/tests/unit/algorithm/select_betta_phi_test.rs
@@ -0,0 +1,106 @@
+#[cfg(test)]
+
+mod select_bet_phi {
+    use debugging::session::debug_session::{Backtrace, DebugSession, LogLevel};
+    use std::{
+        sync::{Arc, Once, RwLock},
+        time::Duration,
+    };
+    use testing::stuff::max_test_duration::TestDuration;
+
+    use crate::{
+        algorithm::{
+            context::{context::Context, ctx_result::CtxResult},
+            entities::bet_phi::BetPhi,
+            initial_ctx::initial_ctx::InitialCtx,
+            select_betta_phi::select_betta_phi::SelectBettaPhi,
+        },
+        kernel::{dbgid::dbgid::DbgId, eval::Eval, storage::storage::Storage},
+    };
+
+    ///
+    ///
+    static INIT: Once = Once::new();
+    ///
+    /// once called initialisation
+    fn init_once() {
+        INIT.call_once(|| {
+            // implement your initialisation code to be called only once for current test file
+        })
+    }
+    ///
+    /// returns:
+    ///  - ...
+    fn init_each() {}
+    ///
+    /// Testing to 'eval()' method
+    #[test]
+    fn eval() {
+        DebugSession::init(LogLevel::Info, Backtrace::Short);
+        init_once();
+        init_each();
+        let dbgid = DbgId("eval".into());
+        log::debug!("\n{}", dbgid);
+        let test_duration = TestDuration::new(&dbgid, Duration::from_secs(1));
+        test_duration.run().unwrap();
+        let test_data = [
+            (
+                1,
+                InitialCtx::new(&mut Storage::new(
+                    "./src/tests/unit/kernel/storage/cache/test_1",
+                ))
+                .unwrap(),
+                CtxResult::Ok(BetPhi {
+                    bet: 0.17,
+                    phi: 1.05,
+                }),
+            ),
+            (
+                2,
+                InitialCtx::new(&mut Storage::new(
+                    "./src/tests/unit/kernel/storage/cache/test_2",
+                ))
+                .unwrap(),
+                CtxResult::Ok(BetPhi {
+                    bet: 0.34,
+                    phi: 1.1,
+                }),
+            ),
+            (
+                3,
+                InitialCtx::new(&mut Storage::new(
+                    "./src/tests/unit/kernel/storage/cache/test_3",
+                ))
+                .unwrap(),
+                CtxResult::Ok(BetPhi {
+                    bet: 0.51,
+                    phi: 1.15,
+                }),
+            ),
+            (
+                4,
+                InitialCtx::new(&mut Storage::new(
+                    "./src/tests/unit/kernel/storage/cache/test_4",
+                ))
+                .unwrap(),
+                CtxResult::Ok(BetPhi {
+                    bet: 0.68,
+                    phi: 1.2,
+                }),
+            ),
+        ];
+        for (step, initial, target) in test_data {
+            let ctx = Arc::new(RwLock::new(Context::new(initial)));
+            let result = SelectBettaPhi::new(ctx.clone()).eval();
+            let result = result.unwrap().read().unwrap().bet_phi.result.clone();
+            assert!(
+                result == target,
+                "step {} \nresult: {:?}\ntarget: {:?}",
+                step,
+                result,
+                target
+            );
+        }
+        test_duration.exit();
+    }
+}
