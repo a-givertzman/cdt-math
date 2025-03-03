a70e856bc1adc4542c0ce4f0004e4b0c9590cf77
a-givertzman <a.givertzman@icloud.com>
2025-03-03 16:25:16 +0300

    RopeCount | Link - listen + test

--- src/tests/unit/kernel/user_setup/user_hook_test.rs
+++ src/tests/unit/kernel/user_setup/user_hook_test.rs
@@ -37,7 +37,7 @@ mod user_hook {
         init_once();
         init_each();
         log::debug!("");
-        let dbg = "test";
+        let dbg = "user_hook_test";
         log::debug!("\n{}", dbg);
         let test_duration = TestDuration::new(dbg, Duration::from_secs(1));
         test_duration.run().unwrap();
