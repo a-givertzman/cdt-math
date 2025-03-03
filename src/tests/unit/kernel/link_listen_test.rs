c1e3f94fc7218e71c0207da40047b426e79d81d2
a-givertzman <a.givertzman@icloud.com>
2025-03-03 14:21:50 +0300

    RopeCount | Link - listen + test

--- src/tests/unit/kernel/link_listen_test.rs
+++ src/tests/unit/kernel/link_listen_test.rs
@@ -1,12 +1,11 @@
 #[cfg(test)]
 
 mod link_listen {
-    use std::{fmt::Debug, sync::{atomic::{AtomicBool, Ordering}, Arc, Once}, time::Duration};
-    use sal_sync::services::entity::name::Name;
-    use serde::{de::DeserializeOwned, Deserialize, Serialize};
+    use std::{sync::Once, time::Duration};
+    use serde::{Deserialize, Serialize};
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
-    use crate::{algorithm::context::ctx_result::CtxResult, kernel::{str_err::str_err::StrErr, sync::link::Link}};
+    use crate::kernel::{str_err::str_err::StrErr, sync::link::Link};
     ///
     ///
     static INIT: Once = Once::new();
@@ -65,69 +64,4 @@ mod link_listen {
     /// Message container
     #[derive(Debug, Serialize, Deserialize, PartialEq)]
     struct Message(pub String);
-    ///
-    /// Receives Query, send associated Reply
-    struct Listener {
-        name: Name,
-        /// recieve and sender channel's
-        link: Option<Link>,
-        /// value to stop thread that await request's
-        exit: Arc<AtomicBool>,
-    }
-    impl Listener {
-        ///
-        /// Struct constructor
-        pub fn new(parent: impl Into<String>, link: Link) -> Self {
-            let name = Name::new(parent, "Listener");
-            Self { 
-                name: name,
-                link: Some(link),
-                exit: Arc::new(AtomicBool::new(false)),
-            }
-        }
-        ///
-        /// Starts service's main loop in the individual task
-        pub async fn run(&mut self) -> Result<tokio::task::JoinHandle<()>, String> {
-            let mut link = self.link.take().unwrap_or_else(|| panic!("{}.run | Link not found", self.name));
-            let dbg = self.name.join();
-            log::info!("{}.run | Starting...", dbg);
-            let exit = self.exit.clone();
-            let listener_handle = tokio::task::block_in_place( move|| {
-                tokio::runtime::Handle::current().spawn(async move {
-                    log::info!("{}.run | Start", dbg);
-                    async fn send_reply(dbg: &str, link: &mut Link, reply: impl Serialize + Debug) {
-                        if let Err(err) = link.send_reply(reply) {
-                            log::debug!("{}.run | Send reply error: {:?}", dbg, err);
-                        };
-                    }
-                    'main: loop {
-                        match link.recv_query::<String>().await {
-                            CtxResult::Ok(query) => match query.as_str() {
-                                "Query-1" => send_reply(&dbg, &mut link, "Reply-1").await,
-                                "Query-2" => send_reply(&dbg, &mut link, "Reply-2").await,
-                                "Query-3" => send_reply(&dbg, &mut link, "Reply-3").await,
-                                "Query-4" => send_reply(&dbg, &mut link, "Reply-4").await,
-                                _ => panic!("Unknown Query: {:?}", query)
-                            }
-                            CtxResult::Err(err) => {
-                                log::warn!("{}.run | Error: {:?}", dbg, err);
-                                break;
-                            }
-                            CtxResult::None => {},
-                        }
-                        if exit.load(Ordering::SeqCst) {
-                            break 'main;
-                        }
-                    }
-                    log::debug!("{}.run | Exit", dbg);
-                })
-            });
-            Ok(listener_handle)
-        }
-        ///
-        /// Sends "exit" signal to the service's thread
-        pub fn exit(&self) {
-            self.exit.store(true, Ordering::SeqCst);
-        }
-    }
 }
